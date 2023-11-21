aa7b49781a2bbfc57204b12daceaf43866b9c673
pavlomorozov <6754248+pavlomorozov@users.noreply.github.com>
2023-11-21 12:30:26 +0100

    Comments improvement on ApplicationContext.

--- injector_example/app_context.py
+++ injector_example/app_context.py
@@ -10,15 +10,18 @@ from injector_example.profiles import Profiles
 class ApplicationContext:
 
     def __init__(self, profile):
+        # Injector instance initiated
         self.__injector = Injector()
-        self.__profile = profile
+        # Profile value can be bound to Injector and later injected while creating managed classes instances.
+        self.__injector.binder.bind(Profiles, profile)
 
-        # The HTTPService implementation defined by profile
+        # The HTTPService implementation defined by profile. This done with binding the HTTPService implementation.
         if Profiles.DEFAULT == profile:
             self.__injector.binder.bind(HTTPService, HTTPDefaultService)
         if Profiles.DEVELOPMENT == profile:
             self.__injector.binder.bind(HTTPService, HTTPDevelopmentService)
 
+        # Injector does not need explicit binding here because we have just one ProcessorService class
         self.__processor_service = self.__injector.get(ProcessorService)
 
     def run(self):
