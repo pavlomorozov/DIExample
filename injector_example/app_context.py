1ea37e93276628e32bab17f3fcaa485e2d1897b1
pavlomorozov <6754248+pavlomorozov@users.noreply.github.com>
2023-11-21 12:15:45 +0100

    Cleanup

--- injector_example/app_context.py
+++ injector_example/app_context.py
@@ -7,20 +7,6 @@ from injector_example.processor_service import ProcessorService
 from injector_example.profiles import Profiles
 
 
-class ApplicationContextModule(Module):
-
-    def __init__(self, profile):
-        self.__profile = profile
-
-    def configure(self, binder):
-
-        binder.bind(Profiles, self.__profile)
-        if Profiles.DEFAULT == self.__profile:
-            binder.bind(HTTPService, HTTPDefaultService)
-        if Profiles.DEVELOPMENT == self.__profile:
-            binder.bind(HTTPService, HTTPDevelopmentService)
-
-
 class Application:
 
     def __init__(self, profile):
